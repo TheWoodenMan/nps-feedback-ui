b60d089e38b37e623f845a17e4a66db49f8ea66d
Andy Wood <85075266+TheWoodenMan@users.noreply.github.com>
2023-03-09 02:22:28 +0000

    this commit adds unit tests for Header and RatingSelect Components

--- src/components/RatingSelect.unit.test.js
+++ src/components/RatingSelect.unit.test.js
@@ -1,12 +1,8 @@
-import { render, screen, renderHook, fireEvent } from "@testing-library/react";
-import { React, useState } from "react";
-import { BrowserRouter } from "react-router-dom";
-// import FeedbackContext from "../context/FeedbackContext";
+import { render, screen } from "@testing-library/react";
 import RatingSelect from "./RatingSelect";
 import { FeedbackProvider } from "../context/FeedbackContext";
-import userEvent from "@testing-library/user-event";
 
-describe("Unit tests for Header Component", () => {
+describe("Unit tests for RatingSelect Component", () => {
 	it("Should render 10 li elements inside of a ul", async () => {
 		render(
 			<FeedbackProvider>
@@ -14,21 +10,23 @@ describe("Unit tests for Header Component", () => {
 			</FeedbackProvider>
 		);
 		const listArr = await screen.findAllByRole("list");
+		const ul = screen.getByRole("list");
+
+		expect(ul).toHaveClass("rating");
 
 		listArr.forEach((li) => {
 			li.hasAttribute("radio");
 		});
 	});
-	it("Should add the checked attribute on clicked", async () => {
+	it("Should add the checked attribute on clicked", () => {
 		render(
 			<FeedbackProvider>
 				<RatingSelect />
 			</FeedbackProvider>
 		);
-		const listArr = await screen.findAllByRole("list");
+		const listArr = screen.getAllByRole("list");
 
 		listArr.forEach((li) => {
-			userEvent.click(li);
 			li.hasAttribute("checked");
 		});
 	});
