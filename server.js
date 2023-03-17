2da97d36eddf1bf445228d9e6aedf6429fbf1b71
Andy Wood <andynwood@gmail.com>
2023-03-17 18:21:58 +0000

    reactivating cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-// const cors = require("cors");
+const cors = require("cors");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const logger = require("morgan");
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
-// app.use(cors());
+app.use(cors());
 
 const clientP = mongoose
 	.connect(dbString)
