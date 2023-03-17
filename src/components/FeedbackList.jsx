06764d9a9854ff57be89870bb66dbce623bd14ab
Andy Wood <andynwood@gmail.com>
2023-03-17 00:02:28 +0000

    added loading spinner removed front end only updates added in backend fetch requests

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,28 +3,30 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useContext } from "react";
 import FeedbackItem from "./FeedbackItem";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function FeedbackList() {
-	const { feedback } = useContext(FeedbackContext);
+	const { feedback, isLoading } = useContext(FeedbackContext);
 
-	if (!feedback || feedback.length === 0) {
+	if (!isLoading && (!feedback || feedback.length === 0)) {
 		return <p>No Feedback Yet</p>;
 	}
-
-	return (
+	return isLoading ? (
 		<div>
+			<Spinner />
+		</div>
+	) : (
+		<div className="feedback-list">
 			<AnimatePresence>
 				{feedback.map((item) => (
-					<div className="feedback-list">
-						<motion.div
-							key={item.id}
-							initial={{ opacity: 0 }}
-							animate={{ opacity: 1 }}
-							exit={{ opacity: 0 }}
-						>
-							<FeedbackItem item={item} feedback={feedback} />
-						</motion.div>
-					</div>
+					<motion.div
+						key={item.id}
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+					>
+						<FeedbackItem item={item} feedback={feedback} />
+					</motion.div>
 				))}
 			</AnimatePresence>
 		</div>
