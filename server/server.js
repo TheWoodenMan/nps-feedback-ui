e6595ea849e6a222ea8a95f25db4544474a1fd75
Andy Wood <andynwood@gmail.com>
2023-03-18 17:08:41 +0000

    error checking routes

--- server/server.js
+++ server/server.js
@@ -38,9 +38,9 @@ app.use(
 
 app.use("/api", feedbackRoutes);
 
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.join(__dirname + "/build/index.html"));
-// });
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname + "/build/index.html"));
+});
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
