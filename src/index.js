65733bef7cba08df56222f2b7a0331e6feef9d22
Andy Wood <andynwood@Andys-MacBook-Pro.local>
2024-12-28 18:35:01 +0000

    backing up before npm

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
-
 const container = document.getElementById("root");
 const root = createRoot(container);
 // const root = document.getElementById("root");
